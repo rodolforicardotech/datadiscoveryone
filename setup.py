e5659221d9cab2c8aa26be65e0c1a62797719a7e
Rodolfo Ricardo <rodolforicardotech@gmail.com>
2021-10-26 20:26:38 -0300

    Diretórios iniciais

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto realizado na formação do Instituto Atlântico - Atlântico Academy Future',
+    author='Grupo Data DiscoveryOne',
+    license='MIT',
+)
